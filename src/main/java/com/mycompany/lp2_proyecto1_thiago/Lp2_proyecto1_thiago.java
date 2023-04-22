81e4c1395d89a52674188c9ff8b2de5a5f51333e
Thiago Gutierrez <thiago.gutierrez70@unae.edu.py>
2023-04-22 00:06:56 -0400

    first commit

---
+++ src/main/java/com/mycompany/lp2_proyecto1_thiago/Lp2_proyecto1_thiago.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lp2_proyecto1_thiago;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Lp2_proyecto1_thiago {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/lp2_proyecto1_thiago/alumnos.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lp2_proyecto1_thiago;
+
+/**
+ *
+ * @author ASUS
+ */
+public class alumnos {
+    int id;
+    String nombre;
+    String apellido;
+    String edad;
+    String direccion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    public void InsertarAlumno(Jtexfield paranombre){
+        
+    }
+}
